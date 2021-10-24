c10533a05257438628b93aa196e35352635dc236
Everson Silva <everson_gsilva@yahoo.com.br>
2021-10-24 08:26:36 -0300

    chore: configuration routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
-import express from 'express';
+import express from "express";
 
-import './database';
+import routes from './routes';
+import "./database";
 
-const use = express();
+const app = express();
 
-use.listen(3333, () => {
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
   console.log("Server started in port 3333");
-})
+});
